203997e94ef3da937ed6636780c1f75415f7f102
Vahagn Poghosyan <vpoghos@gmail.com>
2023-03-02 23:08:08 +0400

    link1

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, SafeAreaView, Linking } from "react-native";
+
+// import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-project-id.firebaseapp.com",
+  databaseURL: "https://your-project-id.firebaseio.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id",
+};
+
+// firebase.initializeApp(firebaseConfig);
+
+const PageOne = ({ setPage }) => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text style={{ fontSize: 20 }}>Page One</Text>
+    <Button title="Go to Page Two" onPress={() => setPage(2)} />
+  </View>
+);
+
+const PageTwo = ({ setPage }) => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text style={{ fontSize: 20 }}>Page Two</Text>
+    <Button title="Go to Page One" onPress={() => setPage(1)} />
+  </View>
+);
 
 export default function App() {
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const handleUrl = async (event) => {
+      if (event.url.endsWith("page2")) {
+        setPage(2);
+      }
+    };
+
+    Linking.addEventListener("url", handleUrl);
+
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        handleUrl({ url });
+      }
+    });
+
+    return () => Linking.removeEventListener("url", handleUrl);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      {page === 1 ? (
+        <PageOne setPage={setPage} />
+      ) : (
+        <PageTwo setPage={setPage} />
+      )}
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
